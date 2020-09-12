4aefc866e8bf87ca5d60e327a1ed3f8b1e76a72e
Kara Temple <kara.temple@bryanuniversity.edu>
2020-09-12 19:26:38 -0400

    updates

--- week-3/changing_styles_wJS/index.js
+++ week-3/changing_styles_wJS/index.js
@@ -7,12 +7,10 @@ for (var i = 0; i < 5; i++) {
     header.style.color = "cornflowerblue";
     header.style.fontWeight = "lighter";
     header.style.fontFamily = "sans serif"; 
-    var className = document.querySelector("#title").className;
-    let ul = document.querySelector("#title");
-    ul.classList.add("border"); 
+     
 }
 
-
-
-
+var className = document.querySelector("#title").className;
+    let ul = document.querySelector("#title");
+    ul.classList.add("border");
 

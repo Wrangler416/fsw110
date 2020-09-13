13201baa91fdc7cafd2d66d3a0aaae23f026b974
Kara Temple <kara.temple@bryanuniversity.edu>
2020-09-13 09:38:35 -0400

    masteryresubmit

--- week-3/creating_a_lis/index.js
+++ week-3/creating_a_lis/index.js
@@ -3,16 +3,23 @@ for (var i = 0; i < 10; i++) {
     title.textContent = "Hello World"; 
     title.setAttribute("id", "hello");
     document.body.append(title); 
+    title.style.color = "blue"; 
 }
-document.getElementById("hello").style.color = "blue";
 
 
+
+var element = document.createElement("ul");
+    element.textContent = "Unordered List"; 
+    element.setAttribute("id", "nameList"); 
+    document.body.append(element); 
 
 const names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"];
 
-var list = document.getElementById("nameList"); 
+
 
 for (var x = 0; x < names.length; x++) {
+    
+    var list = document.getElementById("nameList"); 
     list.innerHTML += "<li>" + names[x] + "</li>" 
 }
 

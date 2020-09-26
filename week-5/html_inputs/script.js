c072a4c32fa7e1c58db7f04447da4ba0cfe4dd6a
Kara Temple <kara.temple@bryanuniversity.edu>
2020-09-26 16:48:59 -0400

    110pnp2

---
+++ week-5/html_inputs/script.js
@@ -0,0 +1,24 @@
+const form = document.form1;
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()  
+});
+
+function myalert() {
+    const first = form.fName.value;
+    const last = form.lName.value;
+    const age = form.age.value; 
+    const gender = form.gender.value; 
+    const cityselect = form.city.value
+    const checkedInput = [];
+        for(let i = 0; i < form.diet.length; i++){
+            if(form.diet[i].checked){
+        checkedInput.push(form.diet[i].value)
+    }
+};
+
+
+    alert(" First Name:" + "" + first + " Last Name:" + "" + last + " Age:" + "" + age + " Gender:" + "" + gender + " City:" + "" + cityselect + " Dietary Restrictions:" + "" + checkedInput)
+}
+
+
